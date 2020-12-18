f1b6c9024e50adf8546ac90f648fbc77cf5c5840
Aliana Britson <Britson.Aliana@deq.state.or.us>
2020-12-17 17:08:22 -0800

    streamlined code

--- ArsenicTranslator.R
+++ ArsenicTranslator.R
@@ -3,11 +3,20 @@
 library(readxl)
 library(tidyverse)
 library(AWQMSdata)
+source("//deqhq1/abrits/GitHub/ShinyNPDES_AWQMS/NPDES_AWQMSQuery.R")
 
 #download all arsenic and inorganic arsenic data from AWQMS from 2000- just looking at effluent for the moment
-source("NPDES_AWQMSQuery.R")
-ars<-NPDES_AWQMS_Qry(startdate="2000-01-01",char=c("Arsenic","Arsenic, Inorganic"),
-                     montype=c("Facility Municipal Sewage (POTW)","Facility Other","Facility Industrial"))
+ars<-NPDES_AWQMS_Qry(startdate="2000-01-01",char=c("Arsenic","Arsenic, Inorganic"))
+
+#remove data without MDL or MRL, unsure if they are ND or not - 
+#unfortunately this removes all of portland harbor's data, but they didn't include MDL or MRL, so I just don't know if it's ND or not
+ars<-subset(ars, !(is.na(ars$MRLValue))& !(is.na(ars$MDLValue)))
+
+#remove data where result numeric equals MDL, it is likely ND...
+ars<-subset(ars,!(ars$Result_Numeric==ars$MDLValue))
+
+#remove any estimated data...either QA/QC issue or J flag (which is low enough that the value is questionable)
+ars<-subset(ars,ars$Result_Type=="Actual")
 
 #convert all to ug/L
 ars<-unit_conv(ars,c("Arsenic","Arsenic, Inorganic"),"mg/l","ug/l")
@@ -22,7 +31,7 @@ ars<-unit_conv(ars,c("Arsenic","Arsenic, Inorganic"),"mg/l","ug/l")
     ars$comb<-paste0(ars$SampleStartDate,",",ars$MLocID)
     
     #get unique identifiers, put into new dataset
-    new<-unique(subset(ars,select=c("act_id","SampleStartDate","MLocID","Org_Name","Char_Name","comb")))
+    new<-unique(subset(ars,select=c("act_id","SampleStartDate","MLocID","MonLocType","Org_Name","Char_Name","comb")))
     
     #select all TR arsenic
     trars<-subset(ars,ars$Sample_Fraction %in% c("Total Recoverable","Total") & ars$Char_Name %in% "Arsenic")
@@ -50,6 +59,20 @@ ars<-unit_conv(ars,c("Arsenic","Arsenic, Inorganic"),"mg/l","ug/l")
     
     #remove all data points where there is no comparison, just keep arsenic so we don't have duplicate rows
     new<-subset(new, !(is.na(new$percinorg))& new$Char_Name %in% "Arsenic")
+    
+    #run stats 
+    arstot<-new%>% group_by(Char_Name,MonLocType) %>%
+        summarise(sample=n(), mean=mean(percinorg),median=median(percinorg),stdev=sd(percinorg),
+                  ninetyth=quantile(percinorg, probs=.9),tenth=quantile(percinorg, probs=.1))
+    
+    ggplot(new,aes(x=Char_Name, y=percinorg,fill=MonLocType,group=MonLocType))+
+        geom_boxplot()+
+        facet_wrap(~Char_Name, scale="free")+
+        scale_y_continuous(limits=c(0,105))+
+        #geom_text(aes(y=0,label=nrow(metgraph)))+
+        labs(x="",y="Percent Inorganic")+
+        theme(axis.text.x=element_blank(),
+              axis.ticks.x=element_blank())
     
 max(new$percinorg)
 min(new$percinorg)
